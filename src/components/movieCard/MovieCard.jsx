956fd858deb119283faae3d426b3f3e70c4bfe8f
Shivam Choudhary <shivamch1704@gmail.com>
2024-03-08 21:45:33 +0530

    Update MovieCard.jsx

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -10,35 +10,38 @@ import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
 const MovieCard = ({ data, fromSearch, mediaType }) => {
-    const { url } = useSelector((state) => state.home);
-    const navigate = useNavigate();
-    let posterUrl = data.poster_path
-        ? url.poster + data.poster_path
-        : PosterFallback;
-    return (
-        <div
-            className="movieCard"
-            onClick={() =>
-                navigate(`/${data.media_type || mediaType}/${data.id}`)
-            }
-        >
-            <div className="posterBlock">
-                <Img className="posterImg" src={posterUrl} />
-                {!fromSearch && (
-                    <React.Fragment>
-                        <CircleRating rating={data.vote_average.toFixed(1)} />
-                        <Genres data={data.genre_ids.slice(0, 2)} />
-                    </React.Fragment>
-                )}
-            </div>
-            <div className="textBlock">
-                <span className="title">{data.title || data.name}</span>
-                <span className="date">
-                    {dayjs(data.first_air_date).format("MMM D, YYYY")} | ({data?.media_type==="tv" ? "TV" : "Movie"})
-                </span>
-            </div>
-        </div>
-    );
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
+  let posterUrl = data?.poster_path
+    ? url.poster + data?.poster_path
+    : PosterFallback;
+  return (
+    <div
+      className="movieCard"
+      onClick={() => navigate(`/${data?.media_type || mediaType}/${data?.id}`)}
+    >
+      <div className="posterBlock">
+        <Img className="posterImg" src={posterUrl} />
+        {!fromSearch && (
+          <React.Fragment>
+            <CircleRating rating={data?.vote_average.toFixed(1)} />
+            <Genres data={data?.genre_ids?.slice(0, 2)} />
+          </React.Fragment>
+        )}
+      </div>
+      <div className="textBlock">
+        <span className="title">{data?.title || data?.name}</span>
+        <span className="date">
+          {dayjs(
+            data?.media_type === "tv"
+              ? data?.first_air_date
+              : data?.release_date
+          ).format("MMM D, YYYY")}{" "}
+          | ({data?.media_type === "tv" ? "TV" : "Movie"})
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
