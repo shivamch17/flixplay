a6feb0dfc0c05e7300ce99ed3c27a65aa0578b12
Shivam Choudhary <shivamch1704@gmail.com>
2024-09-06 14:38:39 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.themoviedb.org/3";
+const BASE_URL = "https://proxy.shivamch17.workers.dev/?url=";
+const API_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-    Authorization: "bearer " + TMDB_TOKEN,
+    Authorization: "Bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
     try {
-        const { data } = await axios.get(BASE_URL + url, {
+        const encodedUrl = encodeURIComponent(API_URL + url);
+        const { data } = await axios.get(`${BASE_URL}${encodedUrl}`, {
             headers,
             params,
         });
+
         return data;
     } catch (err) {
         console.log(err);
