c0b19c6ad6f4deea030127a00358ce8608eb0009
Shivam Choudhary <shivamch1704@gmail.com>
2024-12-05 05:05:36 +0530

    replaced hashrouter with browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 
 import { useSelector, useDispatch } from "react-redux";
